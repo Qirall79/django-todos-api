0e385a8c09e90f958b49c7554b465407a4425ccb
qirall79 <belfatmi79@gmail.com>
2024-06-22 11:39:43 +0100

    Add serializers and views

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Todo
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.todo = Todo.objects.create(
+            title="Test Todo", body="Do some basic testing")
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "Test Todo")
+        self.assertEqual(self.todo.body, "Do some basic testing")
+        self.assertEqual(str(self.todo), self.todo.title)
+        
+    def test_api_listview(self):
+        response = self.client.get(reverse('todo_list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "Test Todo")
+    
+    def test_api_detailview(self): # new
+        response = self.client.get(
+        reverse("todo_detail", kwargs={"pk": self.todo.id}),
+        format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "Test Todo")
